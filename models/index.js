5145c91cd1eeef1be3469c15ee3a2f6a92da43be
VPVelthuizen <vpvelthuizen@gmail.com>
2024-06-06 19:48:26 -0400

    Began the writing of routes

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const Comment = require('./Comment.js');
+const Company = require('./Company.js');
+const Event = require('./Event.js');
+const Forum = require('./Forum.js');
+const Post = require('./Post.js');
+const Tag = require('./Tag.js');
+const User = require('./User.js');
+
+module.exports = {
+    Comment,
+    Company,
+    Event,
+    Forum,
+    Post,
+    Tag,
+    User
+};
