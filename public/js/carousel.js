1bcdf28e327cbd81b75ff6360075daa75b3415ae
Noah Manno <noahmanno19@gmail.com>
2024-06-06 09:43:21 -0400

    adding-homepage

---
+++ public/js/carousel.js
@@ -0,0 +1,28 @@
+// sourced from Stack Overflow user Oliver
+
+//the function below are functions that run after the Dom Content has been loaded
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+    //carousel activation
+    var elems = document.querySelectorAll('.carousel');
+    var xinstances = M.Carousel.init(elems, 
+    {
+      fullWidth: true,
+      indicators: true,
+      numVisible: 3
+    });
+
+    //carousel Next function
+    window.next = function() {
+      var el = document.querySelector(".carousel");
+      var l = M.Carousel.getInstance(el);
+      l.next(1);
+    }
+
+    //carousel previous function
+    window.prev = function() {
+      var el = document.querySelector(".carousel");
+      var l = M.Carousel.getInstance(el);
+      l.prev(1);
+    }
+});
