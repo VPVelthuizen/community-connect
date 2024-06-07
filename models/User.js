f128be2315d169ab09b7c1cd31ca11265dfc2925
sdilolle <stephdilolle@gmail.com>
2024-06-06 20:13:21 -0400

    updated post and user models

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    company: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        reference: {
+            model: 'Company',
+            key: 'company_name',
+          }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
