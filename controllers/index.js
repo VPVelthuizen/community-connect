60922e1fbc3b9ae93949e3a72b2c9a4f834b2e19
Noah Manno <noahmanno19@gmail.com>
2024-06-06 20:11:12 -0400

    adding connection

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const htmlRoutes = require('./htmlRoutes');
 
+// router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
-router.use('/api', apiRoutes);
+
 
 module.exports = router;
