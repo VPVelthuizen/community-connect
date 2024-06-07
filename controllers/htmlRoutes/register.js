d1ee4d6c28ca35c1d9ee954d050f8cd21456e1f9
Noah Manno <noahmanno19@gmail.com>
2024-06-06 21:06:49 -0400

    fixed

--- controllers/htmlRoutes/register.js
+++ controllers/htmlRoutes/register.js
@@ -1,6 +1,6 @@
 const router =  require('express').Router();
 const withoutAuth = require('../../utils/withoutAuth.js');
 
-router.get('/register', withoutAuth, (req, res) => { res.render("register") });
+router.get('/', withoutAuth, (req, res) => { res.render("register") });
 
 module.exports = router;
--- controllers/htmlRoutes/signup.js
+++ controllers/htmlRoutes/signup.js
@@ -1,6 +1,6 @@
 const router =  require('express').Router();
 const withoutAuth = require('../../utils/withoutAuth.js');
 
-router.get('/signup', withoutAuth, (req, res) => { res.render("signup") });
+router.get('/', withoutAuth, (req, res) => { res.render("signup") });
 
 module.exports = router;
