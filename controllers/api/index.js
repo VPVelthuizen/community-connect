5d087a5714336a06f00f6c375f65476e6e41d068
Noah Manno <noahmanno19@gmail.com>
2024-06-12 21:18:07 -0400

    removing tags

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes')
-const tagRoutes = require('./tagRoutes')
 const postRoutes = require('./postRoutes')
 const forumRoutes = require('./forumRoutes')
 const eventRoutes = require('./eventRoutes')
@@ -9,7 +8,6 @@ const companyRoutes = require('./companyRoutes')
 const commentRoutes = require('./commentRoutes')
 
 router.use('/users', userRoutes);
-// router.use('/tags', tagRoutes);
 router.use('/posts', postRoutes);
 router.use('/forums', forumRoutes);
 router.use('/events', eventRoutes);
